cf706777d20f6d57aed6c598ceef1cbb74abd5e0
Azat Khairov <azath_2002@mail.ru>
2021-10-30 11:39:08 +0300

    fix

--- HTML2/scripts/script2.js
+++ HTML2/scripts/script2.js
@@ -4,3 +4,20 @@ function swap(img){
     document.images[0].src = img.src
     img.src = tmp
 }
+position = 4;
+function left()
+{
+    position--;
+    if (position < 1){
+    position = 4
+    }
+    document.images[0].src="../img/img" + position + ".webp";
+}
+function right()
+{
+    position++;
+    if (position > 4){
+    position = 1
+    }
+    document.images[0].src="../img/img" + position + ".webp";
+}
